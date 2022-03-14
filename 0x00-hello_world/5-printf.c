5b20b4f66ccbcb9f0f8e463f8e1dca0e14edfe1d
shukritadic <shukritadicha@gmail.com>
2022-03-14 13:06:57 -0700

    fifth task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *my hello, print task
+ *Return:return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammer, but the output is a piece of art, \n");
+}
