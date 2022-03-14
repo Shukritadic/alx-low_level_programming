84fe508efd6ebda70b420ca06445c67863322d9e
shukritadic <shukritadicha@gmail.com>
2022-03-14 13:12:51 -0700

    commited

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- *main - entry point
- *my hello, print task
- *Return:return 1 after program execution
- */
+*main - entry point
+*my hello, print task
+*Return:return 1 after program execution
+*/
 int main(void)
 {
 printf("with proper grammer, but the output is a piece of art, \n");
+return (0);
 }
