927232e2a3830a9219a44412ac024428a606c650
shukritadic <shukritadicha@gmail.com>
2022-03-16 05:54:38 -0700

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 /**
  *main - entry point
  *
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle \n");
+puts("\"Programming is  like building a multilingual puzzle");
 return (0);
 }
